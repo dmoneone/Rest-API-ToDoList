db330af5ca1d7a7e11e81989c48c5280eef3426d
unknown <kirildim16@gmail.com>
2020-08-06 16:58:01 +0300

    test

--- keys/keys.js
+++ keys/keys.js
@@ -1,5 +1,6 @@
-if(process.env.NODE_ENV === 'production') {
+/*if(process.env.NODE_ENV === 'production') {
     module.exports = require('./keys.prod')
 } else {
     module.exports = require('./keys.dev')
-}
+}*/
+module.exports = require('./keys.dev')
